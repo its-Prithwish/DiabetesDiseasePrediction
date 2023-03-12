ab6299b4f784576e9bedadb4451e4bc05136bc76
Prithwish Kundu <87355004+its-Prithwish@users.noreply.github.com>
2023-03-12 13:24:28 +0530

    Add files via upload

---
+++ DiseasePrediction.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 01:47:25 2023
+
+@author: itspk
+"""
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+#loading the saved model
+
+diabetes_model = pickle.load(open('C:/Users/itspk/OneDrive/Desktop/Disease Prediction System/Saved Models/diabetes_model.sav','rb'))
+
+
+#sidebar for navigation
+with st.sidebar:
+    selected = option_menu('Diabetes Disease Prediction System',['Diabetes Prediction'],default_index=0)
+
+
+#diabetes pred page
+
+if(selected == 'Diabetes Prediction'):
+    #page title
+    st.title('Diabetes Prediction using ML')
+    col1, col2, col3 = st.columns(3)
+    
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+   # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+    
